1f0f8a59f8c8d00772b3ba584670e4bf6d836e67
ali4a <alancacik@gmail.com>
2026-03-01 23:49:43 +0200

    Updated example files

--- example.py
+++ example.py
@@ -12,5 +12,5 @@ print("GET /sensorList", response.json(), "\n", sep = "\n")
 response = httpx.get(f"{BASE_URL}/sensorMinMax", params={"target_date": "2019-07-28"}, headers={"Authorization": f"Bearer {token}"}, timeout = 30)
 print("GET /sensorMinMax", response.json(), "\n", sep = "\n")
 
-response = httpx.get(f"{BASE_URL}/measureFilter", params={"target_sensor_id": 22, "target_metric_id": 1, "time_from": "2019-07-02 01:00:00", "time_to": "2019-07-02 07:00:00", "value_from": 24, "value_to": 26}, headers={"Authorization": f"Bearer {token}"}, timeout = 30)
+response = httpx.get(f"{BASE_URL}/measureFilter", params={"target_sensor_id_list": [22, 34], "target_metric_id_list": 1, "time_from": "2019-07-01 01:00:00", "time_to": "2019-07-01 05:00:00", "value_from": 25, "value_to": 26.5}, headers={"Authorization": f"Bearer {token}"}, timeout = 30)
 print("GET /measureFilter", response.json(), "\n", sep = "\n")
